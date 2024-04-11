b838955274a2a5c8a481d958f1cd6c06f194e068
viniciustvf <vini.tvf@hotmail.com>
2024-04-10 21:27:04 -0300

    atualização

--- src/edu/ifsc/gof/observer/model/ConcreteSubject.java
+++ src/edu/ifsc/gof/observer/model/ConcreteSubject.java
@@ -1,5 +1,28 @@
 package edu.ifsc.gof.observer.model;
 
-public class ConcreteSubject {
+import edu.ifsc.gof.observer.interfaces.Observer;
+import edu.ifsc.gof.observer.interfaces.Subject;
+
+public class ConcreteSubject implements Subject{
+
+    @Override
+    public void register(Observer obs) {
+        
+    }
+
+    @Override
+    public void unregister(Observer obs) {
+        
+    }
+
+    @Override
+    public void notifyObservers() {
+        
+    }
+
+    @Override
+    public Object getupdate(Observer obs) {
+        
+    }
     
 }
